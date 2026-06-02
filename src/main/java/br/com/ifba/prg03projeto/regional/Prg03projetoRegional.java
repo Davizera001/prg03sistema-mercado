fd2209d73173ced4805f5560aa0c1dc3cd7b1d4a
Davizera001 <20242ireads0015@ifba.edu.br>
2026-06-01 23:27:46 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/ifba/prg03projeto/regional/Prg03projetoRegional.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.prg03projeto.regional;
+
+/**
+ *
+ * @author Davi
+ */
+public class Prg03projetoRegional {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
