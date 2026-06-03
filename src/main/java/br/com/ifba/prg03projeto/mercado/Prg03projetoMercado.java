81df7f9cf56cef09ae385b374ca19bb62303ee8d
Davizera001 <20242ireads0015@ifba.edu.br>
2026-06-02 21:53:45 -0300

    refactor: mudança de objetivo do projeto - mercado

--- src/main/java/br/com/ifba/prg03projeto/mercado/Prg03projetoMercado.java
+++ src/main/java/br/com/ifba/prg03projeto/mercado/Prg03projetoMercado.java
@@ -2,15 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package br.com.ifba.prg03projeto.regional;
+package br.com.ifba.prg03projeto.mercado;
 
 /**
  *
  * @author Davi
  */
-public class Prg03projetoRegional {
+
+public class Prg03projetoMercado {
 
     public static void main(String[] args) {
-        System.out.println("Sistema Regional da Sorte iniciado!!");
+        System.out.println("Sistema Mercado iniciado!!!");
     }
 }
