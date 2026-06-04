da1ecbf60f711a11e5a217cafc82aceb75a48e3b
Davizera001 <20242ireads0015@ifba.edu.br>
2026-06-03 23:30:29 -0300

    inicializando projeto Spring boot com JPA e PostgreSQL

---
+++ src/main/java/br/com/ifba/prg03projeto/mercado/Prg03sistemaMercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.prg03projeto.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg03sistemaMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg03sistemaMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifba/prg03projeto/mercado/Prg03sistemaMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.prg03projeto.mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg03sistemaMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
